06537b93168ddf81834c0ef8d10c671895b35fc3
Vaishnavi Naik <71886525+VaishnaviNaik@users.noreply.github.com>
2022-04-07 12:23:41 +0530

    Third section

---
+++ Comparing_2_arrays.rb
@@ -0,0 +1,18 @@
+arr1 = Array.new(3)
+
+arr2 = Array.new(3)
+
+puts "elements of array 1:"
+
+puts arr1
+
+puts "elements of array 2:"
+
+puts arr1
+
+if arr1 == arr2
+    puts "The arrays are equal"
+
+else
+    puts "The arrays are not equal"
+end
