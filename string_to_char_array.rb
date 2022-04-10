7823ef12472ac83cf46439cb0caa578a4de4d809
Vaishnavi Naik <71886525+VaishnaviNaik@users.noreply.github.com>
2022-04-10 20:14:48 +0530

    First Section

---
+++ string_to_char_array.rb
@@ -0,0 +1,15 @@
+# using chars method
+
+str = "HELLO";
+arr = str.chars();
+
+# without using chars method
+
+i=0;
+
+print "Array elements are: \n";
+
+while(i<arr.length())
+    print arr.at(i)," ";
+    i = i + 1;
+end
