0c4bc3c1e59a5da559fe372b86f3286922feece5
Vaishnavi Naik <71886525+VaishnaviNaik@users.noreply.github.com>
2022-04-10 12:56:11 +0530

    First Section

---
+++ concat_string.rb
@@ -0,0 +1,18 @@
+
+#with concat method
+
+str = "Hello"
+   
+str.concat("World")
+   
+puts str
+
+#Without method
+
+puts "Hello World"
+
+a = "My name is"
+b = ", "
+c = "Vaishnavi"
+puts (a + b + c)
+ 
