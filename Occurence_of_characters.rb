4820a38ee519f84cd5a87863eb489e2e560b944e
Vaishnavi Naik <71886525+VaishnaviNaik@users.noreply.github.com>
2022-04-10 20:34:09 +0530

    First section

---
+++ Occurence_of_characters.rb
@@ -0,0 +1,9 @@
+# This does not include upper case letter H
+
+string = 'Hello people, Today is a holiday!'
+string.count('h')
+
+# This does include upper case letter H
+
+string = 'Hello people, Today is a holiday!'
+string.count('h' + 'H')
